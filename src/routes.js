8bc95443a596f66500cb6805ffeea69213000bcf
luiz carlos <luizdetobias@gmail.com>
2025-11-08 00:17:24 -0300

    trocado ordem das rotas

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,8 @@ const router = express.Router()
 router.get('/', taskController.index)
 router.get('/allTasks', taskController.showAll)
 router.get('/newList', taskController.renderNewList)
-router.get('/tasksFromList/:id', taskController.renderList)
-
-
 router.post('/admin/createList', taskController.createNewList)
+router.get('/tasksFromList/:id', taskController.renderList)
 router.post('/admin/deleteTask/:id', taskController.deleteTask)
 router.post('/admin/createNewTask/:id', taskController.createNewTask)
 router.post('/checkTask/:id', taskController.checkTask)
